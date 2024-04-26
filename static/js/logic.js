30aa462b0f8c1524a38864aba0ba3db206aa605c
melissaa06 <147189327+melissaa06@users.noreply.github.com>
2024-04-25 22:01:16 -0400

    fixed colors

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,70 +6,61 @@
   
   
     // Create the map object with options.
-    let map = L.map("map", {
-      center: [40, -98],
-      zoom: 5,
-    });
-    streetmap.addTo(map)
-  
-  function createMarkers(data) {
-  
-//'#F06A6A', '#F0A76A', '#F3B94C', '#F3DB4C', '#E1F34C', '#B6F34C' starting from 90
+let map = L.map("map", {
+  center: [40, -98],
+  zoom: 5,
+});
+streetmap.addTo(map);
 
-//'#B6F34C', '#E1F34C', '#F3DB4C', '#F3B94C', '#F0A76A','#F06A6A' starting from 0
-    function getValue(x) {
-        return x > 90 ? "#F06A6A" :
-               x > 70 ? "#F0A76A" :
-               x > 50 ? "#F3B94C" :
-               x > 30 ? "#F3DB4C" :
-               x > 10 ? "#E1F34C" :
-                   "#B6F34C";
-    }
-    
- 
-    function style(feature) {
-        return {
-            "color": "#000000",
-            "stroke": true,
-            radius: feature.properties.mag*4,
-    fillColor: getValue(feature.geometry.coordinates[2]),
-    weight: 0.25,
-    opacity: 1,
-    fillOpacity: 0.8
-        };
-    }
-    
-    var dat = L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, style(feature));
-        },
-              
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup("<strong>" + feature.properties.place + "</strong><br /><br />Magnitude: " +
-              feature.properties.mag + "<br /><br />Depth: " + feature.geometry.coordinates[2]);
-          }
-      
+function createMarkers(data) {
 
-    });
-    dat.addTo(map);
-
-    let legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-    let limits = [-10, 10, 30, 50, 70, 90];
-    let colors = ['#B6F34C', '#E1F34C', '#F3DB4C', '#F3B94C', '#F0A76A','#F06A6A'];
-
-    for (let i = 0; i < limits.length; i++) {
-        div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
-          + limits[i] + (limits[i + 1] ? "&ndash;" + limits[i + 1] + "<br>" : "+");
-      }
-    return div;
-  };
-
-  // Adding the legend to the map
-  legend.addTo(map);
-
+  function getValue(x) {
+      return x > 90 ? "#47F79F" :
+          x > 70 ? "#F79F47" :
+          x > 50 ? "#F7F747" :
+          x > 30 ? "#9FF747" :
+          x > 10 ? "#47F747" :
+              "#47F79F";
   }
+
+  function style(feature) {
+      return {
+          "color": "#000000",
+          "stroke": true,
+          radius: feature.properties.mag * 4,
+          fillColor: getValue(feature.geometry.coordinates[2]),
+          weight: 0.25,
+          opacity: 1,
+          fillOpacity: 0.8
+      };
+  }
+
+  var dat = L.geoJson(data, {
+      pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng, style(feature));
+      },
+
+      onEachFeature: function (feature, layer) {
+          layer.bindPopup("<strong>" + feature.properties.place + "</strong><br /><br />Magnitude: " +
+              feature.properties.mag + "<br /><br />Depth: " + feature.geometry.coordinates[2]);
+      }
+  });
+  dat.addTo(map);
+
+  let legend = L.control({ position: "bottomright" });
+  legend.onAdd = function () {
+      let div = L.DomUtil.create("div", "info legend");
+      let limits = [-10, 10, 30, 50, 70, 90];
+      let colors = ['#47F79F', '#47F747', '#9FF747', '#F7F747', '#F79F47', '#47F79F'];
+
+      for (let i = 0; i < limits.length; i++) {
+          div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+              + limits[i] + (limits[i + 1] ? "&ndash;" + limits[i + 1] + "<br>" : "+");
+      }
+      return div;
+  };
+  legend.addTo(map);
+}
   
   
   // Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
